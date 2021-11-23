b34cb2b360f82a87ef8a3a39bb5e322b1db480a1
JauniusPinelis <JauniusPinelis@gmail.com>
2021-11-23 20:42:21 +0200

    Finished

--- Console/SampleConsoleAppSolution/SampleConsoleApp/Program.cs
+++ Console/SampleConsoleAppSolution/SampleConsoleApp/Program.cs
@@ -1,33 +1,90 @@
-﻿using SampleConsoleApp;
-using System;
-
-namespace HelloWorld
+﻿namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var dog1 = new Dog();
-            var dog2 = new Dog();
+            // Data types
+            int number = 5;
+            string text = "Text" + " Test";
+            char singleChar = 'A';
+            bool boolValue = false;
+            double doubleValue = 5.432;
 
-            dog1.Bark();
-            dog2.Bark();
+            var arrayOfInts2 = new List<int> { 1, 2, 3, 4, 5 };
 
-            Dog.StaticBark();
+            // Console commands
 
 
-            var randomNumber = GetNumber();
-            PrintInfo(randomNumber);
-        }
 
-        public static void PrintInfo(int info)
-        {
-            Console.WriteLine(info);
-        }
+            // String manipulation example: 5 + 4, 4 - 5;
 
-        public static int GetNumber()
-        {
-            return 5*2;
+            ////Get first two number
+            //consoleInput.Substring(0, 2);
+
+            ////Get first number
+            //consoleInput.FirstOrDefault();
+
+
+
+
+            //// forloop
+            //for (int i = 0; i < consoleElements.Length; i++)
+            //{
+            //    Console.WriteLine(consoleElements[i]);
+            //}
+
+            ////forEach loop
+            //foreach (var consoleElement in consoleElements)
+            //{
+            //    Console.WriteLine(consoleElement);
+            //}
+
+            var isFinished = false;
+
+            while (!isFinished)
+            {
+                Console.WriteLine("Enter your input or enter 'Exit' to exit");
+
+                var consoleInput = Console.ReadLine();
+
+                if (consoleInput == "Exit")
+                {
+                    isFinished = true;
+                }
+                else
+                {
+                    var consoleElements = consoleInput.Split(" ");
+
+                    // try/catch
+                    try
+                    {
+                        var firstInput = consoleElements[0];
+                        var mathOperator = consoleElements[1];
+                        var secondInput = consoleElements[2];
+
+                        var firstNumber = Convert.ToInt32(firstInput);
+                        var secondNumber = Convert.ToInt32(consoleElements[2]);
+
+                        // if statements 
+                        if (mathOperator == "+")
+                        {
+                            Console.WriteLine(firstNumber + secondNumber);
+                        }
+                        if (mathOperator == "-")
+                        {
+                            Console.WriteLine(firstNumber - secondNumber);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("The input is incorrectly formatted");
+                    }
+                }
+
+                // Split string into smaller string by spaces
+
+            }
         }
     }
 }
