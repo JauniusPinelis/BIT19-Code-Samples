3c8111dcb4968eed003ba99d2d3eca63047ceb83
JauniusPinelis <JauniusPinelis@gmail.com>
2021-11-26 19:00:11 +0200

    Updated with the new project

---
+++ Console/SampleConsoleAppSolution/TodoApplication/Models/Todo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication.Models
+{
+    public class Todo
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Completed { get; set; }
+
+        public Todo(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ Console/SampleConsoleAppSolution/TodoApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TodoApplication.Models;
+
+var todos = new List<Todo>();
+
+while (true)
+{
+    Console.WriteLine("Please enter your command: 'Add' or 'List'");
+    var command = Console.ReadLine();
+
+    if (command == "Add")
+    {
+        // Functionality for adding
+        Console.WriteLine("Please enter the name");
+        var name = Console.ReadLine();
+        Console.WriteLine("Please enter the description");
+        var description = Console.ReadLine();
+
+        var todo = new Todo(name, description);
+        todos.Add(todo);
+    }
+    else if (command == "List")
+    {
+        // functionality for listing all todos
+        foreach (var todo in todos)
+        {
+            Console.WriteLine($"Name: {todo.Name}, Description: {todo.Description}");
+        }
+        
+    }
+}
