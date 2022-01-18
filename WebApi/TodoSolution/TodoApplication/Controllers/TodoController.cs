97a4e89d9eb1ade82be04080fe9a107bdd72f3b7
JauniusPinelis <JauniusPinelis@gmail.com>
2022-01-18 20:27:31 +0200

    WebApi

---
+++ WebApi/TodoSolution/TodoApplication/Controllers/TodoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApplication.Data;
+using TodoApplication.Models;
+
+namespace TodoApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public TodoController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public List<Todo> GetAll()
+        {
+            return _dataContext.Todos.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Todo GetById(int id)
+        {
+            return _dataContext.Todos.Find(id);
+        }
+
+        [HttpPost]
+        public void Create(Todo todo)
+        {
+            _dataContext.Todos.Add(todo);
+            _dataContext.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, Todo todoUpdate)
+        {
+            var todo = _dataContext.Todos.Find(id);
+
+            todo.Name = todoUpdate.Name;
+
+            _dataContext.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var todo = _dataContext.Todos.Find(id);
+            _dataContext.Remove(todo);
+            _dataContext.SaveChanges();
+        }
+    }
+
+    
+}
---
+++ WebApi/TodoSolution/TodoApplication/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApplication.Models;
+
+namespace TodoApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
