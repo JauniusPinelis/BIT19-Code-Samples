92c7aad4559bfe9eba61d78e15f8751846af9776
JauniusPinelis <JauniusPinelis@gmail.com>
2022-02-24 21:02:04 +0200

    Integration tests

---
+++ Tests/ShopWithDiscountSolution/tests/ShopWithDiscountProject.IntegrationTests/ShopItemTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using ShopWithDiscountProject.Dtos.ShopItems;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShopWithDiscountProject.IntegrationTests
+{
+    public class ShopItemTests
+    {
+        private HttpClient _client;
+        private CustomWebApplicationFactory<Startup> _factory;
+
+        public ShopItemTests()
+        {
+            _factory = new CustomWebApplicationFactory<Startup>();
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            var createShopItem = new CreateShopItem()
+            {
+                Name = "Candy",
+                Price = 2
+            };
+
+            await _client.PostAsJsonAsync("/shopitem", createShopItem);
+
+            var result = await _client.GetAsync("/shopitem");
+
+            result.EnsureSuccessStatusCode();
+
+            var shopItems = await result.Content.ReadAsAsync<List<ShopItemDto>>();
+
+            shopItems.Count.Should().Be(1);
+            shopItems[0].Name.Should().Be("Candy");
+        }
+    }
+}
--- Tests/ShopWithDiscountSolution/tests/ShopWithDiscountProject.UnitTests/ShopItemServiceTests.cs
+++ Tests/ShopWithDiscountSolution/tests/ShopWithDiscountProject.UnitTests/ShopItemServiceTests.cs
@@ -1,7 +1,10 @@
+using AutoFixture;
 using AutoMapper;
+using FluentAssertions;
 using Moq;
 using ShopWithDiscountProject.Dtos;
 using ShopWithDiscountProject.MappingProfiles;
+using ShopWithDiscountProject.Models;
 using ShopWithDiscountProject.Repositories;
 using ShopWithDiscountProject.Services;
 using System;
@@ -15,6 +18,7 @@ namespace ShopWithDiscountProject.UnitTests
     {
         private readonly ShopItemService _shopItemService;
         private readonly Mock<IShopItemRepository> _shopItemRepositoryMock;
+        private readonly Fixture _fixture = new Fixture();
 
         public ShopItemServiceTests()
         {
@@ -58,30 +62,34 @@ namespace ShopWithDiscountProject.UnitTests
             var price = await _shopItemService.CalculatePriceAsync(buyInformation);
 
             //asert
-            Assert.Equal(100M, price);
+            //Assert.Equal(100M, price);
+            price.Should().Be(100M);
         }
 
         [Fact]
         public async Task CalculatePriceAsync_GivenIncorrectName_ThrowsArgumentException()
         {
             //arrange
-            var exampleShopItem = new Models.ShopItem()
-            {
-                Name = "Candy",
-                Price = 10M
-            };
+            //var exampleShopItem = new Models.ShopItem()
+            //{
+            //    Name = "Candy",
+            //    Price = 10M
+            //};
 
-            var buyInformation = new BuyInformation()
-            {
-                BuyDtos = new List<BuyDto>()
-                {
-                    new BuyDto()
-                    {
-                        ShopItemName = "Candy",
-                        Quantity = 10
-                    }
-                }
-            };
+            //var buyInformation = new BuyInformation()
+            //{
+            //    BuyDtos = new List<BuyDto>()
+            //    {
+            //        new BuyDto()
+            //        {
+            //            ShopItemName = "Candy",
+            //            Quantity = 10
+            //        }
+            //    }
+            //};
+
+            var exampleShopItem = _fixture.Create<ShopItem>();
+            var buyInformation = _fixture.Create<BuyInformation>();
 
             //act
             await Assert.ThrowsAsync<ArgumentException>(() => _shopItemService.CalculatePriceAsync(buyInformation));
