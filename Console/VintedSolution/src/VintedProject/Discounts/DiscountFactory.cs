6f3159881cc1c84ed71176a5023a8f44e57fc22f
JauniusPinelis <JauniusPinelis@gmail.com>
2022-03-16 18:59:18 +0200

    Updates

--- Console/VintedSolution/src/VintedProject/Discounts/DiscountFactory.cs
+++ Console/VintedSolution/src/VintedProject/Discounts/DiscountFactory.cs
@@ -5,11 +5,20 @@ namespace VintedProject.Discounts
 {
     public class DiscountFactory
     {
-        private Dictionary<string, List<ProcessedShipping>> _processedShippings
-            = new Dictionary<string, List<ProcessedShipping>>();
+        private Dictionary<string, List<ProcessedShipping>> _processedShippings = new Dictionary<string, List<ProcessedShipping>>();
 
         public IDiscount Build(ShippingTransaction transaction, List<ShippingInfo> shippingInfos)
         {
+            var key = $"{transaction.Date.Year}-{transaction.Date.Month}";
+            List<ProcessedShipping> transactions = _processedShippings.GetValueOrDefault(key, new List<ProcessedShipping>());
+
+            if (transaction.PackageSize == Enums.PackageSize.L &&
+                transaction.Provider == Enums.ShippingProvider.LP &&
+                transactions.Count(t => t.Size == Enums.PackageSize.L && t.Provider == Enums.ShippingProvider.LP) == 2)
+
+            {
+                return new FreeShippingDiscount();
+            }
             // Logic which will decide which discount to pick
             if (transaction.PackageSize == Enums.PackageSize.S)
             {
@@ -23,13 +32,15 @@ namespace VintedProject.Discounts
         {
             var key = $"{transaction.Date.Year}-{transaction.Date.Month}";
 
-            List<ProcessedShipping> shippings = _processedShippings[key] ?? new List<ProcessedShipping>();
+            List<ProcessedShipping> shippings = _processedShippings.GetValueOrDefault(key, new List<ProcessedShipping>());
             shippings.Add(new ProcessedShipping
             {
                 Discount = transaction.Discount,
                 Provider = transaction.Provider,
                 Size = transaction.PackageSize
             });
+
+            _processedShippings[key] = shippings;
         }
     }
 }
---
+++ Console/VintedSolution/src/VintedProject/Discounts/Strategies/FreeShippingDiscount.cs
@@ -0,0 +1,15 @@
+﻿using VintedProject.Models;
+
+namespace VintedProject.Discounts.Strategies
+{
+    public class FreeShippingDiscount : IDiscount
+    {
+        public void Process(ShippingTransaction transaction)
+        {
+            var discount = transaction.Price.Value;
+
+            transaction.Price = 0;
+            transaction.Discount = discount;
+        }
+    }
+}
