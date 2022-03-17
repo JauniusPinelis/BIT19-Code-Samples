e9810027f6ca0ff9ed753df25a6ed6a6953fcd67
JauniusPinelis <JauniusPinelis@gmail.com>
2022-03-17 20:48:12 +0200

    Reflection demo

---
+++ Console/ReflectionDemo/ReflectionDemo/Model/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace ReflectionDemo.Model
+{
+    public class Todo
+    {
+        [DisplayName]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; } = "Unknown";
+
+        public string Location { get; set; }
+
+        public decimal Test { get; set; }
+    }
+}
---
+++ Console/ReflectionDemo/ReflectionDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using ReflectionDemo.Model;
+
+Console.WriteLine("Hello, World!");
+
+var todos = new List<Todo>();
+
+todos.Add(new Todo { Id = 1, Name = "Todo1", Location = "Vilnius" });
+todos.Add(new Todo { Id = 2, Name = "Todo2" });
+
+foreach (var todo in todos)
+{
+    var properties = todo.GetType().GetProperties();
+
+    var todoPrintInfo = "";
+
+    foreach (var property in properties.Where(p => p.PropertyType == typeof(string)))
+    {
+        todoPrintInfo += $"{property.Name}: {GetPropValue(todo, property.Name)} ";
+    }
+
+
+    Console.WriteLine(todoPrintInfo);
+}
+
+static object GetPropValue(object src, string propName)
+{
+    return src.GetType().GetProperty(propName).GetValue(src, null);
+}
