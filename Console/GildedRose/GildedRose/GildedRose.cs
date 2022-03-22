105b5d77ee19bc1f4cb7ec603114c1714dc3b29e
JauniusPinelis <JauniusPinelis@gmail.com>
2022-03-22 19:52:08 +0200

    Started

---
+++ Console/GildedRose/GildedRose/GildedRose.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (Item item in Items)
+            {
+                if (item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert" || item.Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality++;
+
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Quality < 50)
+                        {
+                            if (item.SellIn < 11)
+                            {
+                                item.Quality++;
+                            }
+
+                            if (item.SellIn < 6)
+                            {
+                                item.Quality++;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (item.Quality > 0)
+                    {
+                        item.Quality--;
+                    }
+                }
+
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn--;
+                }
+
+                if (item.SellIn < 0)
+                {
+                    if (item.Name != "Aged Brie")
+                    {
+                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                item.Quality--;
+                            }
+                        }
+                        else
+                        {
+                            item.Quality = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
